bb419f5d7ebb9a179cc85ba6806114ffe2d966c0
Michael Berger <michael.goetz.berger@telekom.de>
2019-11-27 10:56:41 +0100

    First implementation of Player class

---
+++ src/Player.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Player {
+
+	private ArrayList<Card> myCards = new ArrayList<>();
+
+	private int decideAceValue() {
+		int aceValue = 0;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Du hast ein Ass gezogen. Soll dies jetzt den Wert 1 oder 11 haben?");
+		
+		while (aceValue != 1 && aceValue != 11) {
+			System.out.print("Bitte gebe '1' oder '11' ein: ");
+
+			try {
+				aceValue = input.nextInt();
+			} catch (InputMismatchException e) {
+			}
+		}
+		
+		input.close();
+
+		return aceValue;
+	}
+
+	public void takeCard(Card card) {
+		if (card.getType() == 'A') {
+			card.setValue(this.decideAceValue());
+		}
+	}
+	
+	public void showCards() {
+		for (Card card : myCards) {
+			card.display();
+		}
+	}
+}
