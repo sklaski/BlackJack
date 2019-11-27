61b0cb4007840c961443e797922aea04250d8a89
Steffen Klaski <steffen.klaski@t-systems.com>
2019-11-27 16:29:36 +0100

    showCard in a row

--- src/Participant.java
+++ src/Participant.java
@@ -16,9 +16,9 @@ public abstract class Participant {
 		System.out.println(this.role + " has cards: ");
 		
 		for (Card card : myCards) {
-			 System.out.println(card.display());
+			 System.out.print(card.display() + " ");
 		}
-		
+		System.out.println();
 		System.out.println();
 	}
 	
